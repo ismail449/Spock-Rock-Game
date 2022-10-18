f982599ca5011c8381b1a2cd66e5a11abffe7628
Ismail Khaled <esmael449@stud.fci-cu.edu.eg>
2022-10-18 14:56:01 +0200

    finished the player selection logic

--- script.ts
+++ script.ts
@@ -1,3 +1,50 @@
+const playerScoreEl = document.getElementById('player-score') as HTMLSpanElement
+const playerChoice = document.getElementById('player-choice') as HTMLSpanElement
+const computerScoreEl = document.getElementById('computer-score') as HTMLSpanElement
+const computerChoice = document.getElementById('computer-choice') as HTMLSpanElement
+const resultText = document.getElementById('resultText') as HTMLHeadingElement
+
+const playerRock = document.getElementById('playerRock') as HTMLElement
+const playerPaper = document.getElementById('playerPaper') as HTMLElement
+const playerScissors = document.getElementById('playerScissors') as HTMLElement
+const playerLizard = document.getElementById('playerLizard') as HTMLElement
+const playerSpock = document.getElementById('playerSpock') as HTMLElement
+
+const allGameIcons: NodeListOf<HTMLElement> = document.querySelectorAll('.fa-regular')
+
+//reset all selected icons
+const restSelected = () => {
+  allGameIcons.forEach(icon => icon.classList.remove('selected'))
+}
+
+//passing player selection value
+const select = (choice: string) => {
+  //set all player icons to the original color before make the selected icon black
+  restSelected()
+  playerChoice.textContent = ` --- ${choice}`
+  switch (choice) {
+    case 'rock':
+
+      playerRock.classList.add('selected');
+      break;
+    case 'paper':
+      playerPaper.classList.add('selected');
+      break;
+    case 'scissors':
+      playerScissors.classList.add('selected');
+      break;
+    case 'lizard':
+      playerLizard.classList.add('selected');
+      break;
+    case 'spock':
+      playerSpock.classList.add('selected');
+      break;
+
+    default:
+      break;
+  }
+}
+
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
   paper: { name: 'Paper', defeats: ['rock', 'spock'] },
